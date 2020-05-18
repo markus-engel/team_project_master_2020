a02001706e531d6ff028caa1acbadac9eba9e54f
antschum <schum.antonia@gmail.com>
2020-05-18 15:07:04 +0200

    simplified myEdge, updated adding to graph in MainParserGraph

--- src/myEdge.java
+++ src/myEdge.java
@@ -1,25 +1,13 @@
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 
 public class myEdge {
-    //private Vertex source;              //source of the edge
-    // private Vertex destination;         //destination of the edge
     private UndirectedSparseGraph<myVertex, myEdge> graph;
 
     public myEdge(UndirectedSparseGraph<myVertex, myEdge> graph){
         this.graph=graph;
-        // this.source = source;
-        // this.destination = destination;
     }
 
-   // public Vertex getSource(){ return this.source; }
-
-   // public Vertex getDestination(){ return this.destination; }
-
     public UndirectedSparseGraph<myVertex, myEdge> getGraph(){ return this.graph;}
-
-    //public void setSource(Vertex source){this.source=source;}
-
-    //public void setDestination(Vertex destination){this.destination=destination;}
 
     public void setGraph(UndirectedSparseGraph<myVertex, myEdge> graph) { this.graph = graph; }
 
