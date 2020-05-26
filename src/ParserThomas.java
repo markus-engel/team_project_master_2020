1e05f1b36661f14ba6b609dacdf3b9e6fe393637
ThomasBraun1 <thomas.braun@student.uni-tuebingen.de>
2020-05-26 23:02:51 +0200

    Done. Had to delete original because of some formating issues. Should work well given the format in .txt file.

--- src/ParserThomas.java
+++
@@ -1,4 +0,0 @@
-public class ParserThomas {
-    //Placeholder and Test for github
-    //second test to see if i need to manually push every time
-}
---
+++ src/TaxIdParser.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class TaxIdParser {
+
+    public static void main(String[] args) throws IOException{
+        if(args.length!=1){throw new IOException("Enter single file with contig ID and taxonomic ID!");}
+
+        File file = new File(args[0]);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        String line;
+
+        while((line = br.readLine())!= null){
+            String[] temp = line.split("\t");
+            String conID = temp[0];
+            Integer taxID = Integer.valueOf(temp[1]);
+
+            map.put(conID, taxID);
+        }
+        br.close();
+
+        for(String s : map.keySet()){
+            System.out.println("Contig ID: " +  s + "\t" + "Taxonomic ID: " + map.get(s));
+        }
+    }
+}
