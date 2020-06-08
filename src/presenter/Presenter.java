d89434568affc4ab5fb1eadbf7ea40b57027c2f6
AnKerb <kerber.anna@gmx.de>
2020-06-08 19:18:43 +0200

    GUI presenter idea

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,12 +1,17 @@
 // combines view & model
 package presenter;
 
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.FileChooser;
 import model.Model;
+import model.graph.MyEdge;
+import model.graph.MyVertex;
+import model.io.MainParserGraph;
 import view.View;
 import java.io.File;
+import java.io.IOException;
 
 public class Presenter {
     Model model;
@@ -19,6 +24,7 @@ public class Presenter {
         setUpBindings();
     }
 
+    // Action for the Menu: choose file
     private void setUpBindings() {
         view.getImportMenuItem().setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -28,6 +34,15 @@ public class Presenter {
                 File f = fc.showOpenDialog(null);
                 if (f!=null) {
                     view.setFilenameTextfield("File: " + f.getAbsolutePath());
+                }
+                try {
+                    // the parsing method MainParserGraph.readFile() returns a graph
+                    // is this how to catch the graph correctly to use it further?
+                    UndirectedSparseGraph<MyVertex, MyEdge> currGraph = new UndirectedSparseGraph<MyVertex, MyEdge>();
+                    currGraph = MainParserGraph.readFile(f.getAbsolutePath());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
