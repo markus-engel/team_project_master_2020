bfe5e5152039fdea99c05ad1dd443e0eaaa7d333
Julia-97 <julia.leimeister@student.uni-tuebingen.de>
2020-06-17 15:42:38 +0200

    improvement of TaxonomyTree and namesShort.dmp Task for TaxonomyTree instantiation

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,17 +1,33 @@
 package model;
 
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+import javafx.concurrent.Task;
 import model.graph.MyEdge;
 import model.graph.MyVertex;
 import model.io.GraphParser;
+import model.io.TaxonomyTree;
+
 import java.io.IOException;
 
 public class Model {
 
     private UndirectedSparseGraph<MyVertex, MyEdge> graph;
+    TaxonomyTree currentTaxTree;
 
     public Model() throws IOException {
-        graph = new UndirectedSparseGraph<MyVertex,MyEdge>();
+
+        // Instantiation of the currentTaxTree in a Task to show the responsive GUI already while parsing the tree
+        Task<Void> taskTaxonomyTree = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                currentTaxTree = new TaxonomyTree();
+                System.out.println("Taxonomy tree is loaded");
+                return null;
+            }
+        };
+        new Thread(taskTaxonomyTree).start();
+
+        graph = new UndirectedSparseGraph<MyVertex, MyEdge>();
     }
 
     // create needed objects of the IO classes to use them in presenter
@@ -25,6 +41,5 @@ public class Model {
     public void setGraph(UndirectedSparseGraph<MyVertex,MyEdge> graph) { this.graph = graph;}
 
     //TaxIdParser currentTaxIdParser = new TaxIdParser();
-    //TaxonomyTree currentTaxTree = new TaxonomyTree();
     //currentTaxTree.getRank(9); // as method call example
 }
