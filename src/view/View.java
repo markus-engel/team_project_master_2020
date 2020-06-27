cdfd38922c209ff943a1ed47803dcf57b2231228
jonas <jonas-scheid@live.de>
2020-06-27 12:19:11 +0200

    Updated View

--- src/view/View.java
+++ src/view/View.java
@@ -2,16 +2,12 @@ package view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import com.sun.nio.file.SensitivityWatchEventModifier;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
 
 public class View {
 
@@ -34,25 +30,22 @@ public class View {
     private MenuItem ImportMenuItem;
 
     @FXML
-    private MenuItem SaveMenuItem;
-
-    @FXML
-    private MenuItem CloseMenuItem;
-
-    @FXML
     private MenuItem ImportTaxonomyMenuItem;
 
     @FXML
     private MenuItem ImportCoverageMenuItem;
 
     @FXML
+    private MenuItem SaveMenuItem;
+
+    @FXML
+    private MenuItem CloseMenuItem;
+
+    @FXML
     private Menu helpMenu;
 
     @FXML
     private MenuItem AboutMenuItem;
-
-    @FXML
-    private TextArea ResultTextArea;
 
     @FXML
     private TextField FilenameTextfield;
@@ -62,25 +55,6 @@ public class View {
 
     @FXML
     private TextField OverlapCountTextField;
-
-    @FXML
-    void initialize() {
-        assert Pane != null : "fx:id=\"Pane\" was not injected: check your FXML file 'sample.fxml'.";
-        assert menuBar != null : "fx:id=\"menuBar\" was not injected: check your FXML file 'sample.fxml'.";
-        assert fileMenu != null : "fx:id=\"fileMenu\" was not injected: check your FXML file 'sample.fxml'.";
-        assert ImportMenuItem != null : "fx:id=\"ImportMenuItem\" was not injected: check your FXML file 'sample.fxml'.";
-        assert SaveMenuItem != null : "fx:id=\"SaveMenuItem\" was not injected: check your FXML file 'sample.fxml'.";
-        assert CloseMenuItem != null : "fx:id=\"CloseMenuItem\" was not injected: check your FXML file 'sample.fxml'.";
-        assert ImportTaxonomyMenuItem != null : "fx:id=\"ImportTaxonomyMenuItem\" was not injected: check your FXML file 'gui.fxml'.";
-        assert ImportCoverageMenuItem != null : "fx:id=\"ImportCoverageMenuItem\" was not injected: check your FXML file 'gui.fxml'.";
-        assert helpMenu != null : "fx:id=\"helpMenu\" was not injected: check your FXML file 'sample.fxml'.";
-        assert AboutMenuItem != null : "fx:id=\"AboutMenuItem\" was not injected: check your FXML file 'sample.fxml'.";
-        assert ResultTextArea != null : "fx:id=\"ResultTextArea\" was not injected: check your FXML file 'sample.fxml'.";
-        assert FilenameTextfield != null : "fx:id=\"FilenameTextfield\" was not injected: check your FXML file 'sample.fxml'.";
-        assert SequenceCountTextField != null : "fx:id=\"SequenceCountTextField\" was not injected: check your FXML file 'sample.fxml'.";
-        assert OverlapCountTextField != null : "fx:id=\"OverlapCountTextField\" was not injected: check your FXML file 'sample.fxml'.";
-
-    }
 
     // getter and setter Methods. More have to be implemented if needed
     public MenuItem getImportMenuItem() {
@@ -93,10 +67,6 @@ public class View {
 
     public MenuItem getImportCoverageMenuItem() {
         return ImportCoverageMenuItem;
-    }
-
-    public TextArea getResultTextArea() {
-        return ResultTextArea;
     }
 
     public TextField getFilenameTextfield() {
